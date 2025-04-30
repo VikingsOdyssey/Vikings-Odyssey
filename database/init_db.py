fe9b3ab11c5e01bde79c7f8383b0a69f9543c335
Arthurvs02 <arthurvsdeoliveira@gmail.com>
2025-04-30 00:49:17 -0300

    clean

--- database/init_db.py
+++
@@ -1,5 +0,0 @@
-from .config import Base, engine
-from .models import Jogador
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
--- utils/leitor_texto.py
+++
@@ -1,3 +0,0 @@
-def ler_texto(caminho: str) -> str:
-    with open(caminho, "r", encoding="utf-8") as arquivo:
-        return arquivo.read()
